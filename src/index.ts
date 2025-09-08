6ffe0b76288bf0d176f239962247f65c2110f6bb
ariankoochak <arian.koochak@protonmail.com>
2025-09-08 07:29:27 +0330

    remove temp tests

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-import { LogLoom } from "./core/logger/async";
-import { LogLoomSync } from "./core/logger/sync";
-
 export { LogLoomSync } from "./core/logger/sync";
 export { LogLoom } from './core/logger/async'
-
-
-const log = LogLoomSync({
-    file:{
-        destination: './hellosync',
-        filename: 'log',
-        extension: 'txt',
-    }
-})
-
-log.write('hello')
